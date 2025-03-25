4f90f79637a72073c659ef226b2898c9cd7b7e11
RafaelRSCaldeira <rafael.caldeira.2005@gmail.com>
2025-03-24 21:56:22 -0300

    generated messages now are analyzed by other AI model

--- js/cybersecurity.js
+++ js/cybersecurity.js
@@ -4,18 +4,40 @@ function addNewElement(entry) {
     p.classList.add("mb-2");
     p.textContent = `${entry}`;
     ipList.appendChild(p);
+    ipList.scrollTop = ipList.scrollHeight
+}
+
+function addNewAlert(entry) {
+    let ipList = document.getElementById("analysis");
+    let p = document.createElement("p");
+    p.classList.add("mb-2");
+    p.textContent = `${entry}`;
+    ipList.appendChild(p);
+    ipList.scrollTop = ipList.scrollHeight
+}
+
+function generateMessage() {
+    x = Math.random() * 100;
+    let message = "";
+    if(Math.abs(x) < 1) {
+        message = "Based on the pattern presented in the firewall log file provided to you, generate a new entry that deviates in some way the pattern presented in the log file. The answer shoul contain only the entry, without any other content.";
+    } else {
+        message = "Based on the pattern presented in the firewall log file provided to you, generate a new entry that follows the patter. The entry can not deviates from the pattern presented in the log file in any way. Be aware the IP addresses use specific number ports.";
+    }
+    return message;
 }
 
 async function getData() {
     try {
+        const message = generateMessage();
         const response = await fetch("http://localhost:3001/api/v1/workspace/hackathon-2025/chat", {
             method: "POST",
             headers: {
-              "Content-Type": "application/json", // Corrigido de "text/plain" para "application/json"
+              "Content-Type": "application/json",
               "Authorization": "Bearer K8DHV68-PZG4VRY-G0V4NBJ-2G83MC2"
             },
             body: JSON.stringify({
-                "message": "Following the pattern in the firewall log file, generate a new entry for me.",
+                "message": message,
                 "mode": "chat",
                 "sessionId": "id_cybersecurity",
                 "attachments": []
@@ -24,14 +46,42 @@ async function getData() {
 
         const data = await response.json();
         addNewElement(data.textResponse);
+        analyze(data.textResponse)
         console.log(data.textResponse);
         return data;
     } catch (error) {
         console.error("Erro:", error);
     } finally {
-        // Garante que a função será chamada novamente, mesmo em caso de erro
         setTimeout(getData, 10);
     }
 }
 
 getData();
+
+
+async function analyze(text) {
+    try {
+        const response = await fetch("http://localhost:3001/api/v1/workspace/cybersecurity_analyze/chat", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+              "Authorization": "Bearer K8DHV68-PZG4VRY-G0V4NBJ-2G83MC2"
+            },
+            body: JSON.stringify({
+                "message": `Following the pattern in the firewall log file, analyze the following entry and tell if it deviates from the pattern: ${text}.`,
+                "mode": "chat",
+                "sessionId": "id_cybersecurity",
+                "attachments": []
+              }),
+        });
+
+        const data = await response.json();
+        if(data.textResponse.startsWith("ALERT")) {
+            addNewAlert(`${text} - ${data.textResponse}`)
+        }
+        console.log(data.textResponse);
+        return data;
+    } catch (error) {
+        console.error("Erro:", error);
+    }
+}
